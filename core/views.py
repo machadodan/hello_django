d84ace9a49b1eb554aa77b6e8bf3958d4e274aca
machadodan <danielg_m@hotmail.com>
2021-11-14 17:13:01 -0300

    Commit da primeira aplicação Django. Retornando Hello World

---
+++ core/views.py
@@ -0,0 +1,9 @@
+#from django.shortcuts import render
+
+from django.shortcuts import render, HttpResponse
+
+
+# Create your views here.
+
+def hello(request, nome):
+    return  HttpResponse ('<h1>Hello World {} </h1>'.format(nome))
